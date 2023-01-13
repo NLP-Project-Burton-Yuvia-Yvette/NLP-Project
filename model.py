626c4ee9bdb62870977ff2d7a9f823d00f0163b0
Yuvia Cardenas <yuvia.cardenas1@gmail.com>
2023-01-13 16:26:53 -0600

    edit

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 def model_prep(train,validate,test):
     
     ''' This function takes in data that has been split into train, validate and test and prepares for modeling 
-        returns
     '''
     
     #seperate target
@@ -30,9 +29,11 @@ def cv_countvectorizer(X_train):
 
     return cv
     
-    
+###################################################
+################## Make Models ####################
+###################################################  
 
-    ################## YC #############
+    
 
 def get_tree(x_train, y_train, x_validate, y_validate, x_test, y_test,cv):
     '''
@@ -46,13 +47,17 @@ def get_tree(x_train, y_train, x_validate, y_validate, x_test, y_test,cv):
     tree.fit(X_bow, y_train)
     train_score= tree.score(X_bow, y_train)
     
-    # Whatever transformations we apply to X_train need to be applied to X_test
+    # fit model on validate data
     X_bow_val = cv.transform(x_validate)
     val_score =tree.score(X_bow_val, y_validate)
 
     return train_score, val_score
     
 def get_forest(x_train, y_train, x_validate, y_validate, x_test, y_test,cv):
+    '''
+    Function gets Random Forest model accuracy on train and validate data set 
+    ''' 
+    # fit model on training data
     X_bow1 = cv.fit_transform(x_train)
     rf = RandomForestClassifier(max_depth =6, 
                             min_samples_leaf = 2, 
@@ -60,52 +65,73 @@ def get_forest(x_train, y_train, x_validate, y_validate, x_test, y_test,cv):
     rf.fit(X_bow1, y_train)
     train_score = rf.score(X_bow1, y_train)
     
-    # Whatever transformations we apply to X_train need to be applied to X_test
+    # fit model on validate data
     X_bow_val = cv.transform(x_validate)
     val_score =rf.score(X_bow_val, y_validate)
     
     return train_score, val_score
 
 def get_knn(x_train, y_train, x_validate, y_validate, x_test, y_test,cv):
+    '''
+    Function gets KNN model accuracy on train and validate data set 
+    ''' 
+    # fit model on training data
     X_bow = cv.fit_transform(x_train)
     knn = KNeighborsClassifier(n_neighbors=6, weights='uniform')
     knn.fit(X_bow, y_train)
     train_score = knn.score(X_bow, y_train)
     
-    # Whatever transformations we apply to X_train need to be applied to X_test
+    # fit model on validate data
     X_bow_val = cv.transform(x_validate)
     val_score =knn.score(X_bow_val, y_validate)
     
     return train_score, val_score
 
 
-########################## Model Test Functions ##############################
-
-
 ###################################################
 ################## Evaluate Models ################
 ###################################################
-models = ['Baseline Train', 'SimpleLinear Train', 'GeneralizedLinear Train','Baseline Validate', 'SimpleLinear Validate', 'GeneralizedLinear Validate']
+models = ['DecisionTree_Train', 'DecisionTree_Validate', 'RandomForest_Train', 'RandomForest_Validate', 'KNN_Train', 'KNN_Validate']
 def make_stats_df():
     '''
-    Function creates dataframe for results of pearsonsr statistical 
-    test for all features.
+    Function creates empty dataframe for results of models for evaluation.
     '''
     evaluate_df = pd.DataFrame()
-    evaluate_df['models'] = models
+    evaluate_df['Models'] = models
     return evaluate_df
 
-def final_eval(train, validate, evaluate_df):
-    base_train = baseline_mean_errors(train,validate)
-    simp_train = lm_errors(train)
-    gen_train = glm_errors(train)
-    base_val = baseline_mean_errors(train,validate)
-    simp_val = lm_errors(validate)
-    gen_val = glm_errors(validate)
-
-
-    scores = [base_train, simp_train, gen_train, base_val, simp_val, gen_val]
-    evaluate_df['RMSE']=scores
+def final_eval(DecisionTree_Train, DecisionTree_Validate, RandomForest_Train, RandomForest_Validate, KNN_Train, KNN_Validate, evaluate_df):
+    '''
+    Function adds results of models for evaluation.
+    '''
+    scores = [DecisionTree_Train, DecisionTree_Validate, RandomForest_Train, RandomForest_Validate, KNN_Train, KNN_Validate]
+    evaluate_df['Scores']=scores
     
     return evaluate_df
+###################################################
+################## Model on Test ##################
+###################################################
 
+def get_tree_test(x_train, y_train, x_validate, y_validate, x_test, y_test,cv):
+    '''
+    Function gets Decision Tree model accuracy on train and validate data set 
+    ''' 
+    # create decision tree model using defaults and random state to replicate results
+    tree = DecisionTreeClassifier(max_depth=3)
+
+    # fit model on training data
+    X_bow = cv.fit_transform(x_train)
+    tree.fit(X_bow, y_train)
+    train_score= tree.score(X_bow, y_train)
+    
+    # fit model on validate data
+    X_bow_val = cv.transform(x_validate)
+    val_score =tree.score(X_bow_val, y_validate)
+
+    # fit model on test data
+    X_bow_test = cv.transform(x_test)
+    test_score =tree.score(X_bow_test, y_test)
+
+    #return train_score, val_score, test_score
+    print('Accuracy of Decision Tree classifier model on test set: {:.2f}'
+      .format(tree.score(X_bow_test, y_test)))
